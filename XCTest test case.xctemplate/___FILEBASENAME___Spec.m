1244f698afd613b9922e7cc61cb3afa386ce5ad6
Otavio Cordeiro <contact@otaviocc.com>
2014-04-07 23:37:31 -0700

    Adds XCTest template

---
+++ XCTest test case.xctemplate/___FILEBASENAME___Spec.m
@@ -0,0 +1,29 @@
+#import <XCTest/XCTest.h>
+#import "___FILEBASENAME___.h"
+
+@interface ___FILEBASENAMEASIDENTIFIER___Spec : ___FILEBASENAMEASIDENTIFIER___
+
+@end
+
+@implementation ___FILEBASENAMEASIDENTIFIER___Spec
+
+#pragma mark - Setup
+
+- (void)setUp
+{
+    [super setUp];
+}
+
+- (void)tearDown
+{
+    [super tearDown];
+}
+
+#pragma mark - Tests
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
