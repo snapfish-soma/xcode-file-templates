be5807995399c3a6d5179021050238597214a7af
snapfish-soma <soma-all@snapfish-llc.com>
2015-07-05 20:08:45 -0700

    Update ___FILEBASENAME___Spec.mm

--- Cedar Spec.xctemplate/___FILEBASENAME___Spec.mm
+++ Cedar Spec.xctemplate/___FILEBASENAME___Spec.mm
@@ -1,17 +1,18 @@
-#import "___FILEBASENAME___.h"
+// #import <Cedar/Cedar.h>
+#import "___VARIABLE_cedarClass:identifier___.h"
 
 
 using namespace Cedar::Matchers;
 using namespace Cedar::Doubles;
 
-@interface ___FILEBASENAMEASIDENTIFIER___ (Spec)
+@interface ___VARIABLE_cedarClass:identifier___ (Spec)
 
 @end
 
-SPEC_BEGIN(___FILEBASENAMEASIDENTIFIER___Spec)
+SPEC_BEGIN(___VARIABLE_cedarClass:identifier___Spec)
 
-describe(@"___FILEBASENAMEASIDENTIFIER___", ^{
-    __block ___FILEBASENAMEASIDENTIFIER___ *model;
+describe(@"___VARIABLE_cedarClass:identifier___", ^{
+    __block ___VARIABLE_cedarClass:identifier___ *<#object to test#>;
 
     beforeEach(^{
 		
